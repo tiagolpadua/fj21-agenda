b0331157c68426629f9f20f09605065c5cc672d1
Tiago Pádua <tiagolpadua@gmail.com>
2020-03-06 19:31:34 -0300

    ex9.3

--- src/br/com/caelum/servlet/AdicionaContatoServlet.java
+++ src/br/com/caelum/servlet/AdicionaContatoServlet.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,48 +19,45 @@ import br.com.caelum.agenda.modelo.Contato;
 
 @WebServlet("/adicionaContato")
 public class AdicionaContatoServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        
-        // busca o writer
-        PrintWriter out = response.getWriter();
-        
-        // buscando os parâmetros no request
-        String nome = request.getParameter("nome");
-        String endereco = request.getParameter("endereco");
-        String email = request.getParameter("email");
-        String dataEmTexto = request.getParameter("dataNascimento");
-        Calendar dataNascimento = null;
-        
-        // fazendo a conversão da data
-        try {
-            Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
-            dataNascimento = Calendar.getInstance();
-            dataNascimento.setTime(date);
-        } catch (ParseException e) {
-            out.println("Erro de conversão da data");
-            return; // para a execução do método
-        }
-        
-        // monta um objeto contato
-        Contato contato = new Contato();
-        contato.setNome(nome);
-        contato.setEndereco(endereco);
-        contato.setEmail(email);
-        contato.setDataNascimento(dataNascimento);
-        
-        // salva o contato
-        ContatoDao dao = new ContatoDao();
-        dao.adiciona(contato);
-        
-        // imprime o nome do contato que foi adicionado
-        out.println("<html>");
-        out.println("<body>");
-        out.println("Contato " + contato.getNome() + " adicionado com sucesso");
-        out.println("</body>");
-        out.println("</html>");
-    }
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		// busca o writer
+		PrintWriter out = response.getWriter();
+
+		// buscando os parâmetros no request
+		String nome = request.getParameter("nome");
+		String endereco = request.getParameter("endereco");
+		String email = request.getParameter("email");
+		String dataEmTexto = request.getParameter("dataNascimento");
+		Calendar dataNascimento = null;
+
+		// fazendo a conversão da data
+		try {
+			Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			dataNascimento = Calendar.getInstance();
+			dataNascimento.setTime(date);
+		} catch (ParseException e) {
+			out.println("Erro de conversão da data");
+			return; // para a execução do método
+		}
+
+		// monta um objeto contato
+		Contato contato = new Contato();
+		contato.setNome(nome);
+		contato.setEndereco(endereco);
+		contato.setEmail(email);
+		contato.setDataNascimento(dataNascimento);
+
+		// salva o contato
+		ContatoDao dao = new ContatoDao();
+		dao.adiciona(contato);
+
+		// imprime o nome do contato que foi adicionado
+		RequestDispatcher rd = request.getRequestDispatcher("/contato-adicionado.jsp");
+		rd.forward(request, response);
+	}
 
 }
